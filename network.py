91930e5681699b43598e4a663d17ce2306f2a66e
Dennis Salmanowitz <salmanowitzd@gmail.com>
2021-06-21 17:01:13 -0400

    Update network.py

--- network.py
+++ network.py
@@ -6,39 +6,40 @@ import copy
 def r():
     return random.random()
 
-def get_graph():
-    G = nx.DiGraph()
-    G.add_nodes_from([
-        (0, {"inf": "y", "thresh": r()}),
-        (1, {"inf": "n", "thresh": r()}),
-        (2, {"inf": "n", "thresh": r()}),
-        (3, {"inf": "n", "thresh": r()}),
-        (4, {"inf": "n", "thresh": r()}),
-    ])
-    G.add_weighted_edges_from([
-        (0,1,r()), (0,2,r()), (0,3,r()), (0,4,r()), 
-        (1,2,r()), (1,3,r()), (1,4,r()),
-        (2,3,r()), (2,4,r()),
-        (3,4,r())
-    ])
+def get_graph(n=5, p=0.5):
+    G = nx.fast_gnp_random_graph(n, p, directed=True)
+    for i in G.nodes:
+        G.nodes[i]["inf"] = "n"
+        G.nodes[i]["thresh"] = r()
+    G.nodes[most_suc(G)[0]]["inf"] = "y"
+    for (start, end) in G.edges:
+        G.edges[start, end]["weight"] = r()
     return G
 
 def show(G):
     colors = []
+    #for i in G.nodes:
+    #    inf = G.nodes[i]["inf"]
+    #    if inf < 0.2: colors.append("#ED2938")
+    #    elif inf < 0.4: colors.append("#B25F4A")
+    #    elif inf < 0.6: colors.append("#77945C")
+    #    elif inf < 0.8: colors.append("#3BCA6D")
+    #    else: colors.append("#00FF7F")
     for i in G.nodes:
         colors.append("lightcoral" if G.nodes[i]["inf"] == "n" else "yellowgreen")
     nx.draw(G, with_labels=True, node_color=colors)
     plt.show()
 
+# Cascade
+
 def cascade_step(G, active):
     new = []
-    for i in G.nodes:
-        if G.nodes[i]["inf"] == "y" and i in active:
-            suc = list(G.successors(i))
-            for j in suc:
-                if G.nodes[j]["inf"] == "n" and G[i][j]["weight"] > G.nodes[j]["thresh"]:
-                    G.nodes[j]["inf"] = "y"
-                    new.append(j)
+    for i in active:
+        suc = list(G.successors(i))
+        for j in suc:
+            if G.nodes[j]["inf"] == "n" and G[i][j]["weight"] > G.nodes[j]["thresh"]:
+                G.nodes[j]["inf"] = "y"
+                new.append(j)
     return G, new
 
 def cascade(G, active):
@@ -52,6 +53,8 @@ def cascade(G, active):
         graphs.append(nxt)
     return graphs, newNodes
 
+# Linear Threshold
+
 def lin_thresh_step(G):
     new = []
     for i in G.nodes:
@@ -60,14 +63,14 @@ def lin_thresh_step(G):
             sum = 0
             for j in pred:
                 sum += G[j][i]["weight"]
-            if sum > G.nodes[i]["thresh"]:
+            if sum/len(pred) > G.nodes[i]["thresh"]:
                 G.nodes[i]["inf"] = "y"
                 new.append(i)
     return G, new
 
 def lin_thresh(G):
     graphs = [G]
-    newNodes = [[0]]
+    newNodes = [[most_suc(G)[0]]]
     while True:
         nxt, new = lin_thresh_step(copy.deepcopy(graphs[-1]))
         if new == []:
@@ -75,3 +78,25 @@ def lin_thresh(G):
         graphs.append(nxt)
         newNodes.append(new)
     return graphs, newNodes
+
+# Metrics
+
+def num_influenced(G):
+    count = 0
+    for i in G.nodes:
+        if G.nodes[i]["inf"] > 0.5:
+            count += 1
+    return count
+
+def perc_influenced(G):
+    return num_influenced(G)/len(list(G.nodes))
+
+def most_suc(G):
+    most = 0
+    most_suc = 0
+    for i in G.nodes:
+        n = len(list(G.successors(i)))
+        if n > most_suc:
+            most = i
+            most_suc = n
+    return most, most_suc
