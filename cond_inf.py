1ab20f1dbd6e249322a99ef4f637d64667bd742e
mferguso1 <85973597+mferguso1@users.noreply.github.com>
2021-09-21 22:09:29 -0400

    Updated energy testing and general formatting

--- cond_inf.py
+++ cond_inf.py
@@ -4,10 +4,13 @@ import random
 import copy
 import numpy as np
 import pandas as pd
+import math
+from collections import Counter
 
 def r():
     return random.random()
 
+#Returns a simple directed graph. Used for testing influence spread.
 def get_graph():
     G = nx.DiGraph()
     G.add_nodes_from([
@@ -47,6 +50,7 @@ def get_graph():
     ])
     return G
 
+#Returns a directed graph using n nodes with binomial probability for edges p. Used for testing two-sided influence spread.
 def get_graph1(n=16, p=0.3):
     G = nx.fast_gnp_random_graph(n, p, directed=True)
     for i in G.nodes:
@@ -57,6 +61,8 @@ def get_graph1(n=16, p=0.3):
         G.edges[start, end]["weight"] = 0
     return G
 
+#Creates a graph of the London power network using data from Raman et al
+#Input: area subsection and node limiter (default including all nodes)
 def create_london(area=0, n=0):
     if area < 0 or area > 8:
         print("Invalid area of Greater London")
@@ -68,6 +74,7 @@ def create_london(area=0, n=0):
     edges = subset[["From","To"]].values.tolist()
     return nx.from_edgelist(edges)
 
+#Outputs a network graph visualization for one-sided influence spread.
 def show(G):
     if type(G) != list:
         G = [G]
@@ -83,6 +90,7 @@ def show(G):
         nx.draw(G[j], with_labels=True, node_color=colors)
     plt.show()
     
+#Outputs a network graph visualization for two-sided influence spread. 
 def show1(G):
     if type(G) != list:
         G = [G]
@@ -100,7 +108,8 @@ def show1(G):
             else: colors.append("#F00505")
         nx.draw(G[j], with_labels=True, node_color=colors)
     plt.show()
-
+    
+#Used in prob_inf(): sets the weight of an edge to a connected node j given the influence level of one node i
 def set_edge_weight(G, i, j):
     x = r()
     if G.nodes[i]["inf"] == 3:
@@ -143,6 +152,8 @@ def min_inf(G):
 def med_inf(G):
     return prob_inf(G, "med")
 
+#Influences a graph using maximum, minimum, or median influence propogation.
+#Returns a list of graphs for every step in the algorithm.
 def prob_inf(G, t, max_iter=500):
     if t not in ["max", "min", "med"]:
         print("Invalid type")
@@ -156,12 +167,17 @@ def prob_inf(G, t, max_iter=500):
             break
     return graphs
 
+#Influences the graph for one step.
+#Returns the new graph.
 def prob_inf_step(G, new, t):
     step_list = []
     for i in G.nodes:
         if i not in new:
-            pred = list(G.predecessors(i))
+            #pred = list(G.predecessors(i)) #for directed graphs
+            pred = list(G.neighbors(i)) #for undirected London graph
             for j in pred:
+                if j > i:
+                    continue
                 if j in step_list:
                     return G
                 set_edge_weight(G,j,i)
@@ -169,7 +185,7 @@ def prob_inf_step(G, new, t):
             abs_pw = list(map(abs, pw))
             if sum(abs_pw) == 0:
                 continue
-            else:
+            else: #given the weights of connected edges and an inputted propogation method, assign an influence to the next node
                 if t == "max":
                     G.nodes[i]["inf"] = pw[abs_pw.index(max(abs_pw))]
                 elif t == "min":
@@ -181,7 +197,8 @@ def prob_inf_step(G, new, t):
             new.append(i)
             step_list.append(i)
     return G
-                    
+
+#Used in prob_inf(): Returns the weights of all connected edges from predecessor nodes     
 def pred_weights(G, i, pred, nonzero = True):
     ws = []
     for j in pred:
@@ -190,12 +207,15 @@ def pred_weights(G, i, pred, nonzero = True):
             ws.append(w)
     return ws
 
+#Used in prob_inf(): Returns a list of the nodes that are currently influenced
 def active_nodes(G):
     res = []
     for i in G.nodes:
         if G.nodes[i]["inf"] != 0: res.append(i)
     return res
 
+#Disinfluences a graph using maximum, minimum, or median influence propogation.
+#Returns a list of graphs for every step in the algorithm.
 def disinf(G, t, p, inf, max_iter=500):
     if t not in ["max", "min", "med"]:
        print("Invalid type")
@@ -209,8 +229,9 @@ def disinf(G, t, p, inf, max_iter=500):
         graphs.append(nxt)
         if len(newNodes) == len(G.nodes):
             break
-    return graphs, newNodes
+    return graphs
 
+#Returns a list of chosen inner nodes in graph G to be disinfluenced, given 0<=p<=1 * len(G) inner nodes should be disinfluenced
 def disinf_start_nodes(G, p):
     eligible_nodes = list(filter(lambda i: G.out_degree(i) != 0, G.nodes))
     max_size = len(eligible_nodes)*p//1
@@ -223,6 +244,8 @@ def disinf_start_nodes(G, p):
             count+=1
     return chosen_nodes
 
+#Disinfluences the graph for one step.
+#Returns the new graph.
 def disinf_step(G, new, t):
     step_list = []
     node_list = list(G.nodes)
@@ -249,6 +272,7 @@ def disinf_step(G, new, t):
             step_list.append(i)
     return G
 
+#Returns the weight of an edge to a connected node i given the influence level of one node j
 def disinf_weight(G, i, j):
     x = r()
     if G.nodes[j]["inf"] == G.nodes[i]["inf"]: return G.nodes[j]["inf"]
@@ -267,6 +291,7 @@ def disinf_weight(G, i, j):
     else:
         return G.nodes[i]["inf"]
 
+#Returns a graph reflecting the average influence value of each node given a list of graphs with the same structure
 def average_inf(graphs):
     node_res = [0 for i in graphs[0].nodes]
     for G in graphs:
@@ -277,6 +302,87 @@ def average_inf(graphs):
     for n in avg_graph.nodes:
         avg_graph.nodes[n]["inf"] = round(node_res[n])
     return avg_graph
+
+#Simulates the usage of a battery for an electric car at each 'house' in a network.
+#Returns the list of overloaded nodes.
+#TODO: If a node is overloaded, its successors do not receive any power either.
+def energy_usage(G):
+
+    P_rated = 3.3   #value in kW
+    E_rated = 7        #value in kWH
+    Bat_Efficiency = .85 
+    E_bat = .5 * E_rated #initial amount of energy stored in battery on 1st day
+    delta_t = 1/15
+    P_bat = 0
+    P_bat_list = []       #empty list for graphing energy values later
+    Tx_power = pd.read_csv("Single Day October 31.csv")
+        
+    H = Tx_power["dataid"]                  #unique id for household data
+    d = Tx_power["day number"]              #what day of the year
+    m = Tx_power["time quarter number"]     #what minute of the day 
+
+    dead_nodes = []
+        
+    t = 0  #for loop counter
+
+    for i in G.nodes:
+        t = 0
+        P_bat = 0
+        P_bat_list = []
+        E_bat = .5 * E_rated
+        chargecount = 0
+        for j in Tx_power["time quarter number"]:
+            P_use = Tx_power["grid1"]         #energy used calculated in excel as sum of all eguage values except grid and solar
+            P_gen = Tx_power["solar1"]
+            P_target = P_use[t]-P_gen[t]
+            
+            if P_target > 0 and E_bat >= 0:
+               P_bat = P_target
+            elif P_target < 0 and E_bat <= E_rated:
+                P_bat = max(P_target, -P_rated)
+            else : 
+                P_bat = 0
+
+            #The battery statistics run as normal, but the only factor used in deciding whether to charge or discharge the battery is the level of influence
+            #(the higher the influence, the more likely the battery is charged)
+
+            x=r()
+            if  (G.nodes[i]["inf"] == 3 and x < 0.2) or (G.nodes[i]["inf"] == 2 and x < 0.4) or (G.nodes[i]["inf"] == 1 and x < 0.7) or (G.nodes[i]["inf"] == 0):
+                k = 1/math.sqrt(Bat_Efficiency)     #discharging
+            else:
+                chargecount = chargecount+1;
+                k = math.sqrt(Bat_Efficiency)       #charging
+                
+            E_bat = E_bat - P_bat * k * delta_t
+            t = t + 1
+
+            if chargecount > 1.2*61: #The battery charges 61 times in a normal day. If it charges (example: 20%) more often than normal, it is overloaded.
+                list.append(dead_nodes, i)
+                break
+
+    return dead_nodes
+
+#Runs 100 tests in which the London subnetwork is influenced and electricity output is measured
+#Returns a list of the most commonly overloaded nodes and the number of times they overloaded
+def energy_test_run():
+    c = create_london(0, 50)
+    c.add_edges_from([(8,46), (13, 38), (11, 1), (3, 43), (37, 23), (37, 48), (23, 33), (15, 30)]) #to connect all nodes
+    for i in c.nodes:
+        c.nodes[i]["inf"] = 0
+    c.nodes[7]["inf"] = 3
+    for (start, end) in c.edges:
+        c.edges[start, end]["weight"] = 0
+    all_dead_nodes = []
+    for i in list(range(100)):
+        c = max_inf(c)[-1]
+        dead_nodes = energy_usage(c)
+        list.append(all_dead_nodes, dead_nodes)
+    counter = Counter(all_dead_nodes[0])
+    for i in all_dead_nodes[1:]:
+        counter.update(i);
+
+    return counter.most_common()
+
 
 #Metrics
 
@@ -331,78 +437,3 @@ def cc(G,i):
 
 def most_cc(G):
     return highest_metric(G, cc)
-
-def energy_test_run():
-    c = create_london(0, 50)
-    c.add_edges_from([(0,9), (0, 37), (0, 43), (0, 36), (0, 44), (0, 26), (0, 5), (0, 40)]) #to connect all nodes
-    for i in c.nodes:
-        c.nodes[i]["inf"] = 0
-    c.nodes[0]["inf"] = 3
-    for (start, end) in c.edges:
-        c.edges[start, end]["weight"] = 0
-    c = max_inf(c)[-1]
-    #show(c)
-    o = energy_usage(c)
-    print(o)
-    if o > 0.2*len(c.nodes):
-        print("SHUTDOWN OVERLOAD")
-
-def energy_usage(G):
-    P_rated = 3.3   #value in kW
-    E_rated = 7        #value in kWH
-    Bat_Efficiency = .85 
-    E_bat = .5 * E_rated #initial amount of energy stored in battery on 1st day
-    delta_t = 1/15
-    
-    P_bat = 0
-    P_bat_list = []       #empty list for graphing energy values later
-   
-    Tx_power = pd.read_csv("Single Day October 31.csv")    
-    H = Tx_power["dataid"]                  #unique id for household data
-    d = Tx_power["day number"]              #what day of the year
-    m = Tx_power["time quarter number"]     #what minute of the day 
-    
-    overload = 0
-
-    for i in G.nodes:
-        P_bat = 0
-        for j in Tx_power["time quarter number"]:
-            P_use = Tx_power["grid1"]         #energy used calculated in excel as sum of all eguage values except grid and solar
-            P_gen = Tx_power["solar1"]
-            P_target = P_use[t]-P_gen[t] + 0.5*G.nodes[i]["inf"]
-            
-            if P_target > 0 and E_bat >= 0:
-               P_bat = P_target
-               
-            elif P_target < 0 and E_bat <= E_rated:
-                P_bat = max(P_target, -P_rated)
-                
-            else: 
-                P_bat = 0
-                
-            if P_bat > 0:
-                k = 1/math.sqrt(Bat_Efficiency)     #discharging
-            else : 
-                k = math.sqrt(Bat_Efficiency)       #charging
-                
-            E_bat = E_bat - P_bat * k * delta_t
-
-            if P_bat > P_rated:
-                overload = overload + 1
-                break
-                
-        #Tx_power["Battery Power"] = P_bat_list    #add battery power list to dataframe
-        #print(Tx_power.head())
-        #Tx_power["date"] = Tx_power["date"].astype(str)
-
-
-        #plt.plot("time", "Battery Power", data=Tx_power, color = 'green', marker = '')
-        #plt.plot("time", "solar1", data=Tx_power, color = 'blue', marker = '')
-        #plt.plot("time", "grid1", data=Tx_power, color = 'red', marker = '')
-        #plt.title('Battery Power over time', fontsize = 14)
-        #plt.xlabel("time", fontsize = 14)
-        #plt.ylabel("Power (kW)", fontsize = 14)
-        #plt.legend()
-        #plt.show()
-
-    return overload
